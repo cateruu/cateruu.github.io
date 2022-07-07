97f54668465347fdc7cda1b07717d1c4f07e8dde
Paweł Kromołowski <cater981pl@gmail.com>
2022-07-07 14:59:43 +0200

    folder stucture

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+// styles
+import './css/index.css';
+// components
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
