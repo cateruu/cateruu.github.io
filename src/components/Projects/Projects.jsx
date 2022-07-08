a2942847c1d3a8678ee06cd52999575a8e47d5aa
Paweł Kromołowski <cater981pl@gmail.com>
2022-07-08 13:55:55 +0200

    projects slider

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,16 +4,35 @@ import classes from './css/Projects.module.css';
 // components
 // utils
 import projectsJSON from './utils/projects.json';
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons';
 
 const Projects = () => {
   const [projects, setProjects] = useState(projectsJSON);
 
-  console.log(projects);
+  const sliderElements = projects.map((project) => {
+    return (
+      <p
+        key={project.id}
+        className={`${classes.name} ${project.active ? classes.active : null}`}
+      >
+        {project.name}
+      </p>
+    );
+  });
 
   return (
     <section className={classes.projects}>
       <h2 className={classes.header}>Projects</h2>
-      <div className={classes.container}></div>
+      <div className={classes.container}>
+        <div className={classes.slider}>
+          <FontAwesomeIcon icon={faSortUp} className={classes.move} />
+          {sliderElements}
+          <FontAwesomeIcon icon={faSortDown} className={classes.move} />
+        </div>
+        <div className={classes.project}></div>
+      </div>
       <div className={classes.lDec}></div>
       <div className={classes.mDec}></div>
       <div className={classes.rDec}></div>
