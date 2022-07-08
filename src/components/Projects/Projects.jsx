8cc2c7d4f1c99892e510b227b59030ec837b7610
Paweł Kromołowski <cater981pl@gmail.com>
2022-07-08 13:31:25 +0200

    projects json

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 // styles
 import classes from './css/Projects.module.css';
 // components
+// utils
+import projectsJSON from './utils/projects.json';
 
 const Projects = () => {
+  const [projects, setProjects] = useState(projectsJSON);
+
+  console.log(projects);
+
   return (
     <section className={classes.projects}>
       <h2 className={classes.header}>Projects</h2>
---
+++ src/components/Projects/components/Project.jsx
@@ -0,0 +1,6 @@
+// styles
+import classes from './css/Project.module.css';
+
+const Project = () => {};
+
+export default Project;
